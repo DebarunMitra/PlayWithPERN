fdba327a018bee403ec49ee87d6e1622857f2e75
DebarunMitra <debarun96mitra@gmail.com>
2020-07-19 10:33:19 +0530

    db setup added

--- TodoApp/server/server.js
+++ TodoApp/server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const postGres = require("pg");
 const cors = require("cors");
+const pool = require("./config/db");
 
 const app = express();
 
@@ -8,6 +8,26 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+//Routes
+
+//create todo
+app.post("/todos", async(req, res)=>{
+    try {
+         const {description} = req.body;
+         const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+
+         res.json(newTodo.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+//get todo
+
+//update todo
+
+//delete todo
  
 const PORT = process.env.PORT || 5000 ; 
 
